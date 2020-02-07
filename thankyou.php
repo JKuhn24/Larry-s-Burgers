a8f72cbf514179e357e20bab01cfe33766f2975e
Jeffrey Kuhn <57505584+JKuhn24@users.noreply.github.com>
2020-02-07 10:03:45 -0700

    Update thankyou.php

--- thankyou.php
+++ thankyou.php
@@ -1,5 +1,4 @@
-<?php
-    
+<?php  
     $contactName = filter_input(INPUT_POST, 'name');
     $contactPhone = filter_input(INPUT_POST, 'number');
     $contactMail = filter_input(INPUT_POST, 'mail');
@@ -19,12 +18,16 @@
 
             try {
                 $db = new PDO($dsn, $username, $password);
+                insertIntoDatabase($contactName, $contactPhone, $contactMail, $contactMessage, $employeeID, $db);
 
             } catch (PDOException $e) {
                 $error_message = $e->getMessage();
                 echo "DB Error: " . $error_message; 
                 exit();
             }
+        }
+            //inserts information into database
+            function insertIntoDatabase($contactName, $contactPhone, $contactMail, $contactMessage, $employeeID, $db){
             // Add the product to the database  
             $query = 'INSERT INTO contact
                          (employeeID, contactName, contactPhone, contactMail, contactMessage)
@@ -38,7 +41,7 @@
             $statement->bindValue(':contactMessage', $contactMessage);
             $statement->execute();
             $statement->closeCursor();
-}
+            }
 
 ?>
 <!DOCTYPE html>
@@ -49,12 +52,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta name="description" content="Family run restaurant that focuses mainly on hamburgers." />
     <meta name="keywords" content="food,hamburgers,rastaurant,family" />
-    <link href="css/Style.css" rel="stylesheet">
-    <link href="css/Print.css" rel="stylesheet" media="print">
-    <link rel="apple-touch-icon" sizes="180x180" href="images/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x`6" href="images/favicon-16x16.png">
-    <link rel="manifest" href="images/site.webmanifest">
+    <link href="../css/Style.css" rel="stylesheet">
+    <link href="../css/Print.css" rel="stylesheet" media="print">
+    <link rel="apple-touch-icon" sizes="180x180" href="../images/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="../images/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x`6" href="../images/favicon-16x16.png">
+    <link rel="manifest" href="../images/site.webmanifest">
 </head>
 
 <body>
@@ -81,8 +84,8 @@
     <!-- Footer -->
     <footer>
     <h3>Check out our social media!</h3>
-    <a href="https://www.facebook.com/" target="_blank"><img src="images/facebook.png" alt="facebook logo" /></a>
-    <a href="https://twitter.com/" target="_blank"><img src="images/twitter.png" alt="twitter logo" /></a>
+    <a href="https://www.facebook.com/" target="_blank"><img src="../images/facebook.png" alt="facebook logo" /></a>
+    <a href="https://twitter.com/" target="_blank"><img src="../images/twitter.png" alt="twitter logo" /></a>
     <h3>Contact Us:</h3>
     <p> 11532 W Place Rd. <br/> <br/>
     <a href="">Larrysburgers@gmail.com</a><br/><br/>
