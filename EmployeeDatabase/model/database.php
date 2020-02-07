e8ccc6569cab0e65415fda69a59d463b15b45634
Jeffrey Kuhn <57505584+JKuhn24@users.noreply.github.com>
2020-02-07 10:05:58 -0700

    Update database.php

--- EmployeeDatabase/model/database.php
+++ EmployeeDatabase/model/database.php
@@ -1,12 +1,21 @@
 <?php
-    $dsn = 'mysql:host=localhost;dbname=larrytables';
-    $username = 'root';
-    $password = 'Pa$$w0rd';
+class Database {
+    private static $dsn = 'mysql:host=localhost;dbname=larrytables';
+    private static $username = 'root';
+    private static $password = 'Pa$$w0rd';
+    private static $db;
+    
+    private function __construct() {}
 
+    public static function getDB () {
+    
     try {
-        $db = new PDO($dsn, $username, $password);
+        self::$db = new PDO(self::$dsn, self::$username, self::$password);
     } catch (PDOException $e) {
         echo "You have entered the wrong credentials";
         exit();
     }
+    return self::$db;
+    }
+ }
 ?>
